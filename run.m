e6f0812da2b687fd4d1ecfb54419fc2f844644bf
Cagatay Odabasi <cagatayodabasi91@gmail.com>
2016-06-03 11:58:44 +0300

    Redundant images are removed, run.m is updated

--- run.m
+++ run.m
@@ -25,42 +25,45 @@ initial_xy = [500, 50];
 
 I_fix = [];
 
-I = imread('n12633994_6351.jpeg');
+I = imread('segs0.jpg');
+
+I = im2bw(I);
 
 % Resize the image because the algorithm is optimized for 1024x768 images
-I = imresize(I,768/size(I,1));
+%I = imresize(I,768/size(I,1));
 
 % Divide image into four same sized images
-I_divided = divideImage(I,'four');
+I_divided = divideImage(I,'one');
 
 for i = 1 : size(I_divided,4)
     
     %figure 
     
-    I_divided (:,:,1,i) = medfilt2(I_divided(:,:,1,i),[5 5]);
-    I_divided (:,:,2,i) = medfilt2(I_divided(:,:,2,i),[5 5]);
-    I_divided (:,:,3,i) = medfilt2(I_divided(:,:,3,i),[5 5]);
+    %I_divided (:,:,1,i) = medfilt2(I_divided(:,:,1,i),[5 5]);
+    %I_divided (:,:,2,i) = medfilt2(I_divided(:,:,2,i),[5 5]);
+    %I_divided (:,:,3,i) = medfilt2(I_divided(:,:,3,i),[5 5]);
     
     
-    subplot(2,2,i)
+    %subplot(2,2,i)
     
     
     %tic
-    sal_map = TDApple(I_divided(:,:,:,i));
+    sal_map = I;
+    %sal_map = TDApple(I_divided(:,:,:,i));
     
     minima = findMinima(sal_map, disk_size);    
     %toc
     % preprocessing
-    minima = removeDuplicateMinimum( minima, threshold_minima );
+    %minima = removeDuplicateMinimum( minima, threshold_minima );
     
     %figure
     [apf_modified, path,  fixation_points, I_fix, coverage] = ...
         findPath(sal_map, minima, initial_xy(1), initial_xy(2), sigma,...
         gamma, 1500, I_divided(:,:,:,i));
     
-    analyseFixationImages( I_fix, disk_size)
+    %analyseFixationImages( I_fix, disk_size)
     
-    coverage
+    %coverage
     
 end
 
